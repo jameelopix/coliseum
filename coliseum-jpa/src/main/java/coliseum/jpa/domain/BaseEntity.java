6e5b1d706558f6db7c2446cb4d7b613bda3c1cec
Mohamed Jameel <jameelopix@live.in>
2019-06-18 00:23:49 +0530

    minor modification

--- coliseum-jpa/src/main/java/coliseum/jpa/domain/BaseEntity.java
+++ coliseum-jpa/src/main/java/coliseum/jpa/domain/BaseEntity.java
@@ -14,10 +14,10 @@ import org.springframework.data.annotation.LastModifiedDate;
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Version
-	private Integer version;
+    @Version
+    private Integer version;
 
 //	@CreatedBy
 //	private String createdBy;
@@ -26,46 +26,46 @@ public class BaseEntity implements Serializable {
 //	@Temporal(TemporalType.TIMESTAMP)
 //	private Date createdDate;
 
-	@LastModifiedBy
-	private String lastModifiedBy;
+    @LastModifiedBy
+    private String lastModifiedBy;
 
-	@LastModifiedDate
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date lastModifiedDate;
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
 
-	public String getLastModifiedBy() {
-		return lastModifiedBy;
-	}
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
 
-	public void setLastModifiedBy(String lastModifiedBy) {
-		this.lastModifiedBy = lastModifiedBy;
-	}
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
 
-	public Date getLastModifiedDate() {
-		return lastModifiedDate;
-	}
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
 
-	public void setLastModifiedDate(Date lastModifiedDate) {
-		this.lastModifiedDate = lastModifiedDate;
-	}
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
 
-	public Integer getVersion() {
-		return version;
-	}
+    public Integer getVersion() {
+        return version;
+    }
 
-	public void setVersion(Integer version) {
-		this.version = version;
-	}
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
 
-	/*
-	 * public Date getCreatedDate() { return createdDate; }
-	 * 
-	 * public void setCreatedDate(Date createdDate) { this.createdDate =
-	 * createdDate; }
-	 * 
-	 * public String getCreatedBy() { return createdBy; }
-	 * 
-	 * public void setCreatedBy(String createdBy) { this.createdBy = createdBy; }
-	 */
+    /*
+     * public Date getCreatedDate() { return createdDate; }
+     * 
+     * public void setCreatedDate(Date createdDate) { this.createdDate =
+     * createdDate; }
+     * 
+     * public String getCreatedBy() { return createdBy; }
+     * 
+     * public void setCreatedBy(String createdBy) { this.createdBy = createdBy; }
+     */
 
 }
--- coliseum-service/src/main/java/coliseum/service/FilterUtils.java
+++ coliseum-service/src/main/java/coliseum/service/FilterUtils.java
@@ -1,5 +1,6 @@
 package coliseum.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.collections4.CollectionUtils;
@@ -11,7 +12,7 @@ public class FilterUtils {
     public static void createEqualFilter(List<Filter> filters, String fieldName, List<Object> values) {
         if (CollectionUtils.isNotEmpty(values)) {
             if (values.size() == 1) {
-                filters.add(new Filter(fieldName).by(Operator.EQUALS).with(values.get(0)));
+                checkAndCreateEqualFilter(filters, fieldName, values.get(0));
             } else {
                 filters.add(new Filter(fieldName).by(Operator.IN).with(values));
             }
@@ -23,8 +24,29 @@ public class FilterUtils {
             if (value instanceof List) {
                 createEqualFilter(filters, fieldName, (List) value);
             } else {
-                filters.add(new Filter(fieldName).by(Operator.EQUALS).with(value));
+                checkAndCreateEqualFilter(filters, fieldName, value);
             }
         }
+    }
+
+    private static void checkAndCreateEqualFilter(List<Filter> filters, String fieldName, Object value) {
+        boolean valueExists = false;
+        if (value instanceof String) {
+            String str = (String) value;
+            if (!str.trim().isEmpty()) {
+                valueExists = true;
+            }
+        }
+        if (valueExists) {
+            filters.add(new Filter(fieldName).by(Operator.EQUALS).with(value));
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Filter> filters = new ArrayList<>();
+
+        checkAndCreateEqualFilter(filters, "name", "CATO");
+
+        System.out.println(filters.get(0).toString());
     }
 }
